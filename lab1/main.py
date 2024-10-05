9d7736618e67c5822e61bd62aeb30edcc3601e53
Cristian <cristianprodius1@gmail.con>
2024-10-05 22:15:41 +0300

    extracting the product link and add futher information to scaped product

--- lab1/main.py
+++ lab1/main.py
@@ -1,29 +1,43 @@
 import requests
 from bs4 import BeautifulSoup
 
-# Task 1: Select website and make an HTTP GET request
-url = 'http://books.toscrape.com/'
-response = requests.get(url)
+#Base URL of the page to scrape
+base_url = 'http://books.toscrape.com/'
+response = requests.get(base_url)
 
-# Checking if the response is valid
 if response.status_code == 200:
-    print("Successfully retrieved the webpage")
-    print(f"Content type: {response.headers['Content-Type']}")
+    soup = BeautifulSoup(response.text, 'html.parser')
+    products = []
+    for product in soup.find_all('article', class_='product_pod'):
+        name = product.h3.a['title']
+        price = product.find('p', class_='price_color').text
+        link = product.h3.a['href']
+        products.append({'name': name, 'price': price, 'link': link})
+
+    
+    # Function to scrape the product data futher
+    def scrape_product_data(product_link):
+        response = requests.get(base_url + product_link)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            description = soup.find('meta', {'name': 'description'})['content']
+            availability = soup.find('p', class_='instock availability').text.strip()
+            return {'description': description, 'availability': availability}
+        else:
+            return None
+
+    for product in products:
+        data = scrape_product_data(product['link'])
+        if data:
+            product.update(data)
+
+    # Printts the data
+    for product in products:
+        print(f"Name: {product['name']}")
+        print(f"Price: {product['price']}")
+        print(f"Link: {product['link']}")
+        print(f"Description: {product['description'][:100]}...")  #here is only the firs100 characters
+        print(f"Availability: {product['availability']}")
+        print("---")
 else:
     print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
-    exit()
-
-# Task 2: Using an HTML parser its extracting the name and price of the products
-soup = BeautifulSoup(response.text, 'html.parser')
-
-products = []
-for product in soup.find_all('article', class_='product_pod'):
-    name = product.h3.a['title']
-    price = product.find('p', class_='price_color').text
-    products.append({'name': name, 'price': price})
-
-# Print the extracted products
-for product in products:
-    print(f"Name: {product['name']}")
-    print(f"Price: {product['price']}")
-    print("---")
