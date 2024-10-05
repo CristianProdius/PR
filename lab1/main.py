0a66400965ed5adc17fee9754d356e56c306e1a9
Cristian <cristianprodius1@gmail.con>
2024-10-05 22:04:24 +0300

    changing the website to be scraped and finishing task 3 to extract the name and price of the products

--- lab1/main.py
+++ lab1/main.py
@@ -1,15 +1,29 @@
 import requests
+from bs4 import BeautifulSoup
 
-#the website to be scraped
-url = "https://999.md"
-
-#here I send a request to the website
+# Task 1: Select website and make an HTTP GET request
+url = 'http://books.toscrape.com/'
 response = requests.get(url)
 
+# Checking if the response is valid
 if response.status_code == 200:
-    print("The request was successful")
-    print("Response cotent typeL",  response.headers['content-type'])
-    print("First 200 chars of HTML content: ", response.text[:200])
+    print("Successfully retrieved the webpage")
+    print(f"Content type: {response.headers['Content-Type']}")
 else:
-    print("The request was unsuccessful")
-    print("Status code: ", response.status_code)
+    print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
+    exit()
+
+# Task 2: Using an HTML parser its extracting the name and price of the products
+soup = BeautifulSoup(response.text, 'html.parser')
+
+products = []
+for product in soup.find_all('article', class_='product_pod'):
+    name = product.h3.a['title']
+    price = product.find('p', class_='price_color').text
+    products.append({'name': name, 'price': price})
+
+# Print the extracted products
+for product in products:
+    print(f"Name: {product['name']}")
+    print(f"Price: {product['price']}")
+    print("---")
